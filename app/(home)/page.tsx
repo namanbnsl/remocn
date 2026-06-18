1b78b23dbb50ff8aa484bfff6e2be364fa12698b
dev_wandry <dima.development@wandry.com.ua>
2026-06-18 12:24:52 +0300

    feat: added testimonials home section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,6 +4,7 @@ import { GetStarted } from "./components/sections/get-started";
 import { Hero } from "./components/sections/hero";
 import { InteractiveCode } from "./components/sections/interactive-code";
 import { LandingPartners } from "./components/sections/landing-partners";
+import { Testimonials } from "./components/sections/testimonials";
 import { UiRegistry } from "./components/sections/ui-registry";
 
 export default function Page() {
@@ -15,6 +16,7 @@ export default function Page() {
       <UiRegistry />
       <GetStarted />
       <LandingPartners />
+      <Testimonials />
       <FinalCTA />
     </>
   );
